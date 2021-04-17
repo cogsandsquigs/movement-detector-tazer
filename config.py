2ce95fd7bb54e231b650c0ea4d9d61c1117b57de
Ian Pratt <ianjdpratt@gmail.com>
2021-04-17 00:50:10 -0500

    conor can't format shit

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import serial.tools.list_ports
+
 ports = serial.tools.list_ports.comports()
 i = 0
 for port, desc, hwid in sorted(ports):
@@ -8,16 +9,22 @@ for port, desc, hwid in sorted(ports):
 goodInput = False
 while goodInput == False:
     try:
-        com = int(input("Enter the number corresponding to the port location of the encouragement device: "))
-        if com > i-1:
+        com = int(
+            input(
+                "Enter the number corresponding to the port location of the encouragement device: "
+            )
+        )
+        if com > i - 1:
             print("Not an available port.")
         else:
             goodInput = True
     except:
         print("Must be int.")
 try:
-    with open('config.txt', 'w') as f:
+    with open("config.txt", "w") as f:
         f.write(sorted(ports)[com][0])
     print("Encouragement device location set to " + sorted(ports)[com][0] + ".")
-except: 
-    print("Error setting encouragement device at location " + sorted(ports)[com][0] + ".")
+except:
+    print(
+        "Error setting encouragement device at location " + sorted(ports)[com][0] + "."
+    )
