23feb6d66ce07a962ac338b7fe6d4790153e740b
Ian Pratt <ianjdpratt@gmail.com>
2021-04-17 10:34:00 -0500

    rm uneccissary comments

--- python/main.py
+++ python/main.py
@@ -27,7 +27,6 @@ def EncourageWrapper(data=1):
     for i in range(encourage_threshould):
         # replace pass with encourage(data) to actually encourage
         encourage(data)
-        # pass
 
 
 def GetBaseVideo(indx):
