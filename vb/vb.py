76520969525687a29c98ddedff1d5f374ec7b473
Ian Pratt <ianjdpratt@gmail.com>
2021-04-17 03:10:53 -0500

    rm vb for now

--- vb/vb.py
+++ vb/vb.py
@@ -1,4 +1,5 @@
 from random import *
 
 for i in range(1000):
-    f = open(str(i) + "owo" + str(random()) + ".vb", "x")
+    with open(str(i) + "owo" + str(random()) + ".vb", "w") as f:
+        f.write("""Create a new text file!""")
